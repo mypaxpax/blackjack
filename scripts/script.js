59574c7c077db61639f164384cc9888fed397fad
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-15 16:12:11 +0100

    player object

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
 
 let sum = 0;
 let cards = [];
 let hasBlackjack = false;
 let isAlive = false;
 let message = "";
+
+let player = {
+  name: "Bob",
+  chips: 195,
+};
+
+playerEl.textContent = player.name + " $" + player.chips;
 
 function getRandomCard() {
   let random = Math.floor(Math.random() * 13) + 1;
